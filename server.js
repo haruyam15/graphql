48ded293e5aa96a86d3397d0789ed04804f9b2eb
Haeun <haruyam15@gmail.com>
2025-01-16 16:29:35 +0900

    Mutation 추가

--- server.js
+++ server.js
@@ -5,23 +5,49 @@ const app = express();
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
 
+const users = [
+  { id: '1', name: '하은' },
+  { id: '2', name: 'Bob' },
+];
+
 const schema = buildSchema(`
   type Query {
-    description : String
+    user : [User]
+  }
+  type User {
+    id : String
     name : String
-    }
-  `);
+  }
+  input UpdateUserInput {
+    id: String
+    name: String
+  }
+  type Mutation {
+    updateUser(input:UpdateUserInput) : User
+  }
+`);
 
-const root = {
-  description: '안녕하세요',
-  name: '하은',
+const rootValue = {
+  user: () => {
+    return users;
+  },
+  updateUser: ({ input }) => {
+    const { id, name } = input;
+    const user = users.find((user) => user.id === id);
+    if (!user) {
+      throw new Error('해당 아이디와 일치하는 사용자가 없습니다.');
+    }
+    if (name) user.name = name;
+    return user;
+  },
 };
 
 app.use(
   '/graphql',
   graphqlHTTP({
     schema: schema,
-    rootValue: root,
+    rootValue: rootValue,
+    graphiql: true,
   })
 );
 
